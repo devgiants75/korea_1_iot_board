afc8275fe4bb6d3dd742d9a7b5818248c5665d51
devgiants75 <devgiants75@naver.com>
2024-11-13 10:31:04 +0900

    20241113

--- board_front/src/App.tsx
+++ board_front/src/App.tsx
@@ -3,13 +3,15 @@ import RootLayout from './components/rootLayout/RootLayout'
 import RootContainer from './components/rootContainer/RootContainer'
 import Todo from './views/Todo';
 import { Route, Routes } from 'react-router-dom';
+import MyPage from './views/myPage/MyPage';
 
 export default function App() {
   return (
     <RootLayout>
       <RootContainer>
         <Routes>
-          <Route path='/' element={ <Todo /> } />
+          <Route path='/todo' element={ <Todo /> } />
+          <Route path='/' element={ <MyPage /> } />
         </Routes>
       </RootContainer>
     </RootLayout>
